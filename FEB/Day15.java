d07c96d686716a188d6201380de75eeead42bb07
purniiiima <purniiiima@gmail.com>
2024-02-25 11:03:00 +0530

    Maximum SubArray

---
+++ FEB/Day15.java
@@ -0,0 +1,12 @@
+class Day15 {
+    public int maxSubArray(int[] nums) {
+         int current = nums[0];
+         int max = nums[0];
+         for(int i=1; i<nums.length; i++){
+             current = Math.max(nums[i], current + nums[i]);
+             max = Math.max(current,max);
+         }
+         return max;
+        
+    }
+}
